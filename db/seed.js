0148478b559b50389c4081a497c249afb1955504
Danielle Cavinder <daniellecavinder@gmail.com>
2023-02-22 20:01:13 -0700

    added some seed data

---
+++ db/seed.js
@@ -0,0 +1,51 @@
+const connection = require('../config/conection');
+const { Profile, Thoughts } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+    await Profile.deleteMany({});
+    await Thoughts.deleteMany({});
+
+    const profileSet = [
+        {
+            username: 'JimmyK',
+            email: 'jimmy@email.com',
+        },
+        {
+            username: 'SusanG',
+            email: 'susan@email.com',
+        },
+        {
+            username: 'CoolHat24',
+            email: 'coolhat@email.com',
+        }
+    ]
+
+    const thoughtsSet = [
+        {
+            thoughtText: 'testing, testing 1,2,3...!',
+            username: 'susanG'
+        },
+        {
+            thoughtText: 'Happy Wednesday!',
+            username: 'CoolHat24',
+        },
+        {
+            thoughtText: 'Please let me pass this assignment',
+            username: 'SusanG',
+        },
+        {
+            thoughtText: 'Let me see if this works',
+            username: 'JimmyK'
+        },
+    ]
+    
+    await Profile.collection.insertMany(profileSet)
+    await Thoughts.collection.insertMany(thoughtsSet)
+
+  console.info('Seed completed');
+  process.exit(0);
+});
